705a071ab9c25576bd15e7d3bd30f12b8bd24a7b
Swayam Samyak Debasis <1234swayamsamyak@gmail.com>
2021-01-29 20:12:29 +0530

    added project

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import SearchIcon from "@material-ui/icons/Search";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
   margin: {
+    margin: theme.spacing(1),
+  },
+  button: {
     margin: theme.spacing(1),
   },
 }));
@@ -27,6 +31,9 @@ const Main = () => {
               color="primary"
             />
           </Grid>
+          <Button variant="outlined" color="primary">
+            Search
+          </Button>
         </Grid>
       </div>
     </div>
